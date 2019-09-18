dc4d75a3a7c130c1e2ca0dd299bbfab57ac6e918
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-09-18 13:50:18 -0300

    fix conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "user#index"
+  root to: 'user#index'
 
   resources :promotions, only: %i[index show new create] do
     resources :cupons, only: %i[create]
   end
 
   resources :administrator, only: %i[index]
-  resources :pay_methods, only: %i[new create]
-  resources :single_classes,only: %i[show]
+  resources :pay_methods, only: %i[index new create]
+  resources :single_classes, only: %i[show]
 
   resources :registrations, only: %i[index new create show edit update] do
     resources :single_classes, only: %i[new create]
--- spec/requests/api_requests_payments_by_cpf_spec.rb
+++ spec/requests/api_requests_payments_by_cpf_spec.rb
@@ -1,31 +1,21 @@
 require 'rails_helper'
 
 describe 'Api requests all payments details for one client' do
-    it 'successfully' do
-        unity = Unity.create!(name: 'Paulista')
-        pay_method = PayMethod.create!(name: 'Boleto')
-        plan = Plan.create!(name: 'Executivo', value: 70)
-        registration = Registration.create!(name: 'teste', cpf: "222", \
-                                            unity_id: 1, plan_id: 1, \
-                                            pay_method_id: 1)
-        payment = registration.payments.create!(value: 50, registration_id: 1,\
-                                               pay_method_id: 1,\
-                                               dt_venc: '2019-09-14 18:20:44')
+  it 'successfully' do
+    registration = create(:registration, cpf: '222')
+    registration.generate_anual_payments
 
-        get "/api/v1/registrations/payments", params: {cpf: '222'}
-        json_response = JSON.parse(response.body, symbolize_names: true)
+    get '/api/v1/registrations/payments', params: { cpf: '222' }
 
-        expect(response.status).to eq 200
-        expect(response.body).to include 'registration_id'
-        expect(response.body).to include 'status'
-        expect(response.body).to include 'value'
-    end
+    expect(response.status).to eq 200
+    expect(response.body).to include 'registration_id'
+    expect(response.body).to include 'status'
+    expect(response.body).to include 'value'
+  end
 
-    it 'search by invalid cpf' do
-        get "/api/v1/registrations/payments/", params: { cpf: '100'}
-        json = JSON.parse(response.body, symbolize_names: true)
+  it 'search by invalid cpf' do
+    get '/api/v1/registrations/payments/', params: { cpf: '100' }
 
-        expect(response.status).to eq 404
-    end
-
+    expect(response.status).to eq 404
+  end
 end
